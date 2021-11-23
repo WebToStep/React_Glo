abdf41379d0d4bd9102ba0bc44cc115118e7cd8e
WebStep <webtostep@yandex.kz>
2021-11-23 19:43:14 +0500

    react lesson 02 test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/style.css';
+
+import Header from './modules/Header'
+import Main from './modules/Main'
+import Features from './modules/Features'
+import Contact from './modules/Contact'
+import Footer from './modules/Footer'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+     <Header />
+     <Main />
+     <Features />
+     <Contact />
+     <Footer />
+    </>
+  ); 
 }
 
 export default App;
---
+++ src/modules/Header.js
@@ -0,0 +1,17 @@
+import '../css/style.css';
+
+const Header = () => {
+   return (
+		<header>
+			<div className="wrapper">
+				<div className="header">
+					<a href="#"><div className="header-logo"></div></a>
+					<a href="tel:888" className="header-phone"></a>
+					<div className="header-phonelink"><a href="tel:7(962)556-1234">+7(962)556-1234</a></div>
+				</div>
+			</div>
+		</header>
+   )
+}
+
+export default Header
